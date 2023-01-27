f2b68c44b6c3c327282ae19aec473a94c2a924cf
Marco <marf_mejiao@unicah.edu>
2023-01-26 18:57:39 -0600

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,10 @@ router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
  });
 
+ router.get('/version', (_req, res) => {
+  const version: string = "1.0.0";
+  const jsonResp= {"name":"FODA Be","version": version};
+  res.json(jsonResp);
+ });
+
 export default router;
